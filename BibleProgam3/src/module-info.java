ed76046058209bae06e31d4e7f548bf300b8189a
klemnic <129095392+klemnic@users.noreply.github.com>
2023-11-21 20:38:47 -0600

    Add files via upload

---
+++ BibleProgam3/src/module-info.java
@@ -0,0 +1,8 @@
+/**
+ * 
+ */
+/**
+ * 
+ */
+module BibleProgam3 {
+}
---
+++ BibleProgam3/src/net/codejava/Human.java
@@ -0,0 +1,11 @@
+package net.codejava;
+
+public class Human {
+	public static int MaxAge = 72;
+	public static int AgeAsApprentice = 18;
+	public static int YearToBeDisciple = 3;
+	public static int AgeOfGivingBirth = 30;
+	public static int CapOfApprentice = 2;
+	public static int CapOfBaby = 3; // I updated this to represent the average number of children per woman
+	public static double BaseBirthRate = 0.02; // 2% base birth rate
+}
